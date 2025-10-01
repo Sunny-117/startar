cbeb7ea8514b96b521dff62762d462431b75867f
Sunny-117 <zhiqiangfu6@gmail.com>
2025-10-01 12:46:25 +0800

    chore: deps

--- build.config.ts
+++ build.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
       // https://github.com/vitejs/vite/pull/14030
       // we can always use non-transpiled code since we support node 18+
       prompts: 'prompts/lib/index.js',
-    },
-    minify: true
+    }
   }],
 })
